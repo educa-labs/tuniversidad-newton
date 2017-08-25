770b52feacba44b6235bfcb5f11e3f0c0f667b2b
Andres <afcadiz@uc.cl>
2017-08-25 17:44:39 -0300

    started forest clasifier implementation

---
+++ newton/forest/classifier.py
@@ -0,0 +1,15 @@
+import numpy as np
+import os
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.externals import joblib
+
+class Forest:
+
+    def __init__(self, area_id):
+        pass
+
+    def query(self, points, n_results):
+        pass
+
+if __name__ == '__main__':
+    print("do some shit")
