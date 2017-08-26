c0ce35fde0196117b59faec405a9762a6369cb79
Andres <afcadiz@uc.cl>
2017-08-26 12:03:12 -0300

    changed rf training to carreer_id classes and made Fores package

--- newton/forest/classifier.py
+++ newton/forest/classifier.py
@@ -1,15 +1,28 @@
 import numpy as np
-import os
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.externals import joblib
 
 class Forest:
 
     def __init__(self, area_id):
-        pass
+        self.area_id = area_id
+        self.classifier = joblib.load('serialized/{}_data'.format(area_id))
 
     def query(self, points, n_results):
-        pass
+        predicted = self.classifier.predict_proba(points)
+        result = []
+        for i in range(predicted.shape[0]):
+            best = np.argpartition(predicted[i],-n_results)[-n_results:]
+            result.append(best)
+        return result
+
+    def get_class(self,index):
+        return self.classifier.classes_[index]
+
 
 if __name__ == '__main__':
-    print("do some shit")
+
+    rf = Forest(10)
+    q = rf.query([[800,700,0,700,720]],4)
+    print(q)
+    print(rf.get_class(q))
